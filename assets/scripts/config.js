07bb558f4e144fbff28af5fdfd37bf20b3af8242
kstrickland0612 <kstrickland0612@gmail.com>
2019-04-30 09:18:52 -0400

    Add Heroku production URL to config file

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://sheltered-cliffs-69470.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
